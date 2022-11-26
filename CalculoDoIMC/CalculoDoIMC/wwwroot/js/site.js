1180e671ba3cb9a2cfa2a8515b67e197fb96125e
Raphael Francisco Alves <raphaelbatealves@gmail.com>
2022-11-26 19:46:52 -0300

    Javascript

--- CalculoDoIMC/CalculoDoIMC/wwwroot/js/site.js
+++ CalculoDoIMC/CalculoDoIMC/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function myFunction() {
+    const message = document.getElementById("p01");
+    message.innerHTML = "";
+    let x = document.getElementById("Altura").value;
+    let y = document.getElementById("Peso").value;
+    let imc = " ";
+    try {
+        if (x == "" || y == "") throw "empty";
+        if (isNaN(x) || isNaN(y)) throw "not a number";
+        x = Number(x);
+        y = Number(y);
+        imc = y / (x * x);
+        document.getElementById("p02").innerHTML = imc.toFixed(1);
+    }
+    catch (err) {
+        message.innerHTML = "Input is " + err;
+    }
+    if (imc < 20) {
+        document.getElementById("p03").innerHTML = `O seu imc é de ${imc.toFixed(1)} e indica que você esta abaixo do peso.`
+    }
+    else if (imc >= 20 && imc <= 25) {
+        document.getElementById("p03").innerHTML = `O seu imc é de ${imc.toFixed(1)} que é considerado normal.`            
+    }
+    else if (imc > 25 && imc <= 30) {
+        document.getElementById("p03").innerHTML = `O seu IMC é de ${imc.toFixed(1)} indicando que você esta com excesso de peso.`
+            
+    }
+    else if (imc > 30 && imc <= 35) {
+        document.getElementById("p03").innerHTML = `O seu IMC é de ${imc.toFixed(1)} indicando obesidade.`
+    }
+    else {
+            document.getElementById("p03").innerHTML = `O seu IMC é de ${imc.toFixed(1)} indicando obesidade mórbida.`
+    }
+}
